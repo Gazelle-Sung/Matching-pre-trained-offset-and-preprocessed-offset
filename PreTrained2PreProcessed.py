24d34b3a2426b69739012f38a99bb30eaa751d4f
Mingyou Sung <mingyousung@gmail.com>
2022-04-13 09:36:29 -0400

    Update PreTrained2PreProcessed.py

--- PreTrained2PreProcessed.py
+++ PreTrained2PreProcessed.py
@@ -3,6 +3,7 @@ import torch
 import os
 import numpy as np
 
+from text_processing import preprocessing
 
 class Clause_feature:
     def __init__(self, tk_idx, tk, offset, pos, ner, dep, bert_offset):
@@ -14,17 +15,6 @@ class Clause_feature:
         self.dep = dep
         self.bert_offset = bert_offset
 
-# *** Select a preprocessing method
-def sel_preprocessing(_input="stanza"):
-    # 1. Stanza
-    if _input.lower() == "stanza":
-        import stanza
-        return stanza.Pipeline('en')
-
-    elif _input.lower() == "spacy":
-        # 2. Spacy
-        import spacy
-        return spacy.load("en_core_web_sm")
 
 
 # *** Select a pre-trained Word Embeddings
@@ -57,57 +47,10 @@ def sel_pretrained(_input="bert"):
             glove = pickle.load(file)
         return None, glove
 
-# *** Tokenize with selected method and store with token-level id, text, offset, pos tag, ner and dependency triple
-def tokenization_processing(_datasample, nlp, _pipeline='stanza'):
-    cur_clause_feature = []
-    # Spacy
-    if _pipeline == "spacy":
-        print("****** Selected preprocessing method: {}".format(_pipeline))
-        doc = nlp(_datasample)
-        for tk_idx, tk in enumerate(doc):
-            cur_tk = tk.text
-            cur_id = tk_idx+1
-            cur_head = tk.head.i
-
-            cur_tk_start_offset = tk.idx
-            cur_tk_end_offset = tk.idx + len(tk)
-
-            cur_pos = tk.pos_
-            cur_ner = tk.ent_type_
-            cur_dep = tk.dep_
-
-            cur_dep_triple = (cur_id, cur_dep, cur_head)
-
-            cur_clause_feature.append(Clause_feature(cur_id, cur_tk, (cur_tk_start_offset, cur_tk_end_offset), cur_pos, cur_ner, cur_dep_triple, ''))
-    # Stanza
-    elif _pipeline == "stanza":
-        print("****** Selected preprocessing method: {}".format(_pipeline))
-        doc = nlp(_datasample)
-        for sen in doc.sentences:
-            for tk in sen.tokens:
-                tk_infor_dict = tk.to_dict()[0]
-                cur_tk = tk_infor_dict["text"]
-                cur_id = tk_infor_dict['id']
-                cur_head = tk_infor_dict['head']
-
-                offsets = tk_infor_dict['misc'].split("|")
-                cur_tk_start_offset = int(offsets[0].split("=")[1])
-                cur_tk_end_offset = int(offsets[1].split("=")[1])
-
-                cur_pos = tk_infor_dict["xpos"]
-                cur_ner = tk_infor_dict["ner"]
-
-                cur_dep = tk_infor_dict["deprel"]
-
-                cur_dep_triple = (cur_id, cur_dep, cur_head)
-
-                cur_clause_feature.append(Clause_feature(cur_id, cur_tk, (cur_tk_start_offset, cur_tk_end_offset), cur_pos, cur_ner, cur_dep_triple, ''))
-    return cur_clause_feature
-
 
 def embedding2preprocessed(args, datasample, nlp, embedder, model):
     # preprocessed result
-    preprocessed_result = tokenization_processing(datasample, nlp, args.pipeline)
+    preprocessed_result = preprocessing(datasample, args.pipeline)
 
     if args.emb == "bert":
         # bert tokenized result
