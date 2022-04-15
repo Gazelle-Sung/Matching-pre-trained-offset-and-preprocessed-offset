ac67acca652b83b0800740f5f7ea4ddbbfc533c6
Mingyou Sung <mingyousung@gmail.com>
2022-04-15 07:29:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import OffsetMatching2Preprocessed as pt2pp
 
 def main(args):
     sample_text = """Officials are set to announce details of B.C.'s latest restart plan on Tuesday as daily case counts continue to trend downward and hours after the last round of "circuit breaker" restrictions expired."""
-    model, embedder = pt2pp.sel_pretrained(args.emb)
+    model, embedder = pt2pp.sel_pretrained(args.lm, args.pm)
 
     result = pt2pp.embedding2preprocessed(args, sample_text, embedder, model)
 
@@ -24,7 +24,19 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--pipeline", default="spacy", type=str, help="Preprocessing method such as Stanza or Spacy.")
-    parser.add_argument("--emb", default="bert", type=str, help="Pre-trained word embedding such as BERT, ELMo or Glove.")
+    parser.add_argument("--lm", default="bert", type=str, help="Pre-trained language model such as BERT, ELMo or Glove.")
+    """
+    BERT, RoBERTa, ELMo, Glove
+    """
+    parser.add_argument("--pm", default="bert-base-uncased", type=str, help="Pre-trained model of the selected language model such as bert-base-uncased for BERT, 42b & 840b for Glove.")
+    """
+    1. BERT (https://huggingface.co/transformers/v3.3.1/pretrained_models.html)
+     - bert-base-uncased, bert-large-uncased, bert-base-cased, ...
+    2. RoBERTa (https://huggingface.co/transformers/v3.3.1/pretrained_models.html)
+     - roberta-base, roberta-large, ...
+    3. Glove (https://nlp.stanford.edu/projects/glove/)
+     - 42b, 840b, ...
+    """
     parser.add_argument("--output_type", default="frozen", type=str, help="The output type of either frozen word embedding or BERT input for BERT embedding.")
 
     args = parser.parse_args()
